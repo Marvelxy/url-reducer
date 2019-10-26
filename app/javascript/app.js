9757d3edcc53eedde336ad2ee42b99b02b446218
Marvelous Akpotu <still4marvelous@gmail.com>
2019-10-27 00:27:55 +0100

    Add scroll to top of page.

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	
+	$('#reduce_url_link').on('click', function(){
+		$('html, body').animate({ scrollTop: $('#container_fluid').offset().top }, '2000');
+	});
+
+	$('#reduce_url_link').on('click', function(){
+		$('html, body').animate({ scrollTop: $('#container_fluid').offset().top }, '2000');
+	});
 });
