5e503b5b1774e9a211e67539c712a892700a1a18
Marvelous Akpotu <still4marvelous@gmail.com>
2019-10-27 00:30:50 +0100

    Add scroll animation for 'How it works sections.'

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		$('html, body').animate({ scrollTop: $('#container_fluid').offset().top }, '2000');
 	});
 
-	$('#reduce_url_link').on('click', function(){
-		$('html, body').animate({ scrollTop: $('#container_fluid').offset().top }, '2000');
+	$('#how_it_works_link').on('click', function(){
+		$('html, body').animate({ scrollTop: $('#how_it_works').offset().top }, '2000');
 	});
 });
