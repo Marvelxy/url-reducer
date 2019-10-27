21709600e777003b049e9f2ca888dfb6600d012a
Marvelous Akpotu <still4marvelous@gmail.com>
2019-10-27 19:47:23 +0100

    Add click event and keyboard enter event to the reduce component.

--- app/javascript/components/Reduce.js
+++ app/javascript/components/Reduce.js
@@ -4,17 +4,26 @@ import PropTypes from "prop-types";
 class Reduce extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      url: ''
+    }
   }
 
   componentDidMount(){
 
   }
 
-
-
   componentWillUnmount() {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
+  }
+
+  setURL = (event) => {
+    this.setState({url: event.target.value})
+  }
+
+  reduce = () => {
+    alert(this.state.url);
   }
 
   render () {
@@ -29,14 +38,30 @@ class Reduce extends React.Component {
     };
 
     return (
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Enter long URL"
+          aria-label="Enter long URL"
+          aria-describedby="basic-addon2"
+          style={reduceInputStyle}
+          onChange={this.setURL}
+          onKeyPress={(event) => {event.key === 'Enter' ? this.reduce() : ''}}
+        />
 
-
-          <div className="input-group mb-3">
-            <input type="text" className="form-control form-control-lg" placeholder="Enter long URL" aria-label="Enter long URL" aria-describedby="basic-addon2" style={reduceInputStyle} />
-            <div className="input-group-append">
-              <button type="button" name="button" className="btn btn-light" style={reduceButtonStyle}>Reduce</button>
-            </div>
-          </div>
+        <div className="input-group-append">
+          <button
+            type="button"
+            name="button"
+            className="btn btn-light"
+            style={reduceButtonStyle}
+            onClick={this.reduce}
+          >
+            Reduce
+          </button>
+        </div>
+      </div>
     );
   }
 }
