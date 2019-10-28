5a0c6fab2e239452f0608a4465182e009d9b91e6
Marvelous Akpotu <still4marvelous@gmail.com>
2019-10-28 17:01:32 +0100

    Add loader when the reduce button is clicked.

--- app/javascript/components/Reduce.js
+++ app/javascript/components/Reduce.js
@@ -5,7 +5,8 @@ class Reduce extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: ''
+      url: '',
+      spinner: false
     }
   }
 
@@ -14,16 +15,40 @@ class Reduce extends React.Component {
   }
 
   componentWillUnmount() {
-    Events.scrollEvent.remove('begin');
-    Events.scrollEvent.remove('end');
+    /*Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');*/
   }
 
   setURL = (event) => {
     this.setState({url: event.target.value})
   }
 
+  showSpinner = () => {
+    this.setState({spinner: true})
+  }
+
+  hideSpinner = () => {
+    this.setState({spinner: false})
+  }
+
   reduce = () => {
-    alert(this.state.url);
+    this.showSpinner();
+    const BASE_URL = 'localhost:3000/bookmarks.json';
+    fetch('/bookmarks.json', {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      }
+     })
+    .then(response => response.json())
+    .then(json => {
+      this.hideSpinner();
+      this.setState({items: json});
+
+      console.log(this.state);
+    });
   }
 
   render () {
@@ -37,29 +62,61 @@ class Reduce extends React.Component {
       fontWeight: '400',
     };
 
-    return (
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control form-control-lg"
-          placeholder="Enter long URL"
-          aria-label="Enter long URL"
-          aria-describedby="basic-addon2"
-          style={reduceInputStyle}
-          onChange={this.setURL}
-          onKeyPress={(event) => {event.key === 'Enter' ? this.reduce() : ''}}
-        />
+    const reduceLoaderStyle = {
+      color: '#000000'
+    }
 
-        <div className="input-group-append">
-          <button
-            type="button"
-            name="button"
-            className="btn btn-light"
-            style={reduceButtonStyle}
-            onClick={this.reduce}
-          >
-            Reduce
-          </button>
+    return (
+      <div>
+        {/*<div className="spinner-grow text-light text-center mb-2" role="status">
+          <span className="sr-only">Loading...</span>
+        </div> */}
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Enter long URL"
+            aria-label="Enter long URL"
+            aria-describedby="basic-addon2"
+            style={reduceInputStyle}
+            onChange={this.setURL}
+            onKeyPress={(event) => {event.key === 'Enter' ? this.reduce() : ''}}
+          />
+
+          <div className="input-group-append">
+            {(() => {
+              if(this.state.spinner === false){
+                return(
+                  <button
+                    type="button"
+                    name="button"
+                    className="btn btn-light"
+                    style={reduceButtonStyle}
+                    onClick={this.reduce}
+                  >
+                    Reduce
+                  </button>
+                )
+              }
+              else{
+                return(
+                  <button
+                    type="button"
+                    className="btn btn-light"
+                    style={reduceLoaderStyle}
+                    disabled
+                  >
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    Reducing...
+                  </button>
+                )
+              }
+            })()}
+          </div>
         </div>
       </div>
     );
