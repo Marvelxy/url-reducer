e7ff6de27c73ec200fa0c51c856d652040ba623d
Marvelous Akpotu <still4marvelous@gmail.com>
2019-10-31 21:38:38 +0100

    Add custom messages for validation.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,9 @@
 class Url < ApplicationRecord
-  validates :short, uniqueness: true
+  validates :short, uniqueness: {
+    #message: "should happen once per year"
+  }
+
   validates :long, uniqueness: {
-    message: "should happen once per year"
+    message: "This URL has already been reduced by another user."
   }
 end
