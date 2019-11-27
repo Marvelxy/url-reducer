2add3becb2fe7a5110fdea8ea2f6944b6fdd5f12
Marvelous Akpotu <still4marvelous@gmail.com>
2019-11-27 22:29:32 +0100

    Add proper code to url edit

--- app/javascript/components/Url.js
+++ app/javascript/components/Url.js
@@ -110,8 +110,9 @@ class Url extends React.Component {
           let old_saved_urls = this.state.saved_urls;
           let edited_url = this.state.current_url_on_edit
           //let new_saved_urls = old_saved_urls[edited_url].long = this.state.editLongURL.url.trim();
-          let new_saved_urls = old_saved_urls[edited_url].long.trim();
+          old_saved_urls[edited_url].long = this.state.editLongURL.url.trim();
           this.setState({saved_urls: old_saved_urls});
+          //console.log([...this.state.saved_urls])
         }
       });
     }
