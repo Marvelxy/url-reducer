3b1f1c564cb3f40f7a58eb5d920f6874725f08d2
Marvelous Akpotu <still4marvelous@gmail.com>
2019-12-03 18:52:23 +0100

    Add passing tests to url_spec

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -8,15 +8,12 @@ RSpec.describe Url, type: :model do
   end
 
   it "has one after adding one if user is signed in" do
-    #user = User.create(username: 'Marv', email: 'marv@example.com', password: 'marv_marv')
-    #category = Category.create(name: 'Shopping Mall', description: 'A general shopping mall', marker_icon: 'kkkjjkkj')
-
     Url.create!(
-      long: 'Test user',
-      short: '4.987558578, 2.87979778798',
-      user: user
+      short: 'Test user',
+      long: '4.987558578, 2.87979778798',
+      user_id: 2
     )
-    expect(Bookmark.count).to eq 1
+    expect(Url.count).to eq 1
   end
   
   it "has one after adding one if user is not signed in" do
