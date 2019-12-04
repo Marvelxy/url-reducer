2bf52d5fc555deedcac21cd161461cdfbdf67f30
Marvelous Akpotu <still4marvelous@gmail.com>
2019-12-04 23:11:56 +0100

    Working on pagination.

--- app/javascript/components/Url.js
+++ app/javascript/components/Url.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Modal, Button, Form, Alert} from 'react-bootstrap';
-//import  EmptyURLList from '../images/No_list_found.png';
+import Pagination from "./Pagination";
 
 class Url extends React.Component {
   constructor(props) {
@@ -255,35 +255,41 @@ class Url extends React.Component {
                   else{
                     return (
                       <div>
-                      {
-                        this.state.saved_urls.map((url, index) => (
-                          <div
-                            key={index}
-                            className="pt-3 pl-3 pr-3 pb-1 url-item"
-                          >
-                            <div>
-                              Long URL: <span className="">{url.long}</span>
+                        {
+                          this.state.saved_urls.map((url, index) => (
+                            <div
+                              key={index}
+                              className="pt-3 pl-3 pr-3 pb-1 url-item"
+                            >
+                              <div>
+                                Long URL: <span className="">{url.long}</span>
+                              </div>
+                              <div>
+                                Short URL:&nbsp;
+                                <a href={"http://url-reduzer.herokuapp.com/r/" + url.short}>
+                                  http://url-reduzer.herokuapp.com/r/{url.short}
+                                </a>
+                              </div>
+                              <div className="btn-group btn-group-sm mt-2" role="group" aria-label="Basic example">
+                                <button type="button" className="btn btn-light text-primary" onClick={(e) => this.show(e,index)}>
+                                  <i className="fas fa-edit fa-xs"></i> Edit
+                                </button>
+                                <button type="button" className="btn btn-light text-success" onClick={(e) => this.regenerateURL(e,index)}>
+                                  <i className="fas fa-redo fa-xs"></i> Regenerate
+                                </button>
+                                <button type="button" className="btn btn-light text-danger" onClick={(e) => this.deleteURL(e,index)}>
+                                  <i className="fas fa-trash fa-xs"></i> Delete
+                                </button>
+                              </div>
                             </div>
-                            <div>
-                              Short URL:&nbsp;
-                              <a href={"http://url-reduzer.herokuapp.com/r/" + url.short}>
-                                http://url-reduzer.herokuapp.com/r/{url.short}
-                              </a>
-                            </div>
-                            <div className="btn-group btn-group-sm mt-2" role="group" aria-label="Basic example">
-                              <button type="button" className="btn btn-light text-primary" onClick={(e) => this.show(e,index)}>
-                                <i className="fas fa-edit fa-xs"></i> Edit
-                              </button>
-                              <button type="button" className="btn btn-light text-success" onClick={(e) => this.regenerateURL(e,index)}>
-                                <i className="fas fa-redo fa-xs"></i> Regenerate
-                              </button>
-                              <button type="button" className="btn btn-light text-danger" onClick={(e) => this.deleteURL(e,index)}>
-                                <i className="fas fa-trash fa-xs"></i> Delete
-                              </button>
-                            </div>
-                          </div>
-                        ))
-                      }
+                          ))
+                        }
+                        <Pagination
+                          itemsperpage={5}
+                          nocolumns={4}
+                          items={this.state.saved_urls}
+                          pagesspan={4}
+                        />
                       </div>
                     )
                   }
