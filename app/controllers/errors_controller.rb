304fd900ad3e6eab47d6326b0ee7e05160d967f1
Marvelous Akpotu <still4marvelous@gmail.com>
2019-12-31 23:44:42 +0100

    Add error actions.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,2 +1,22 @@
 class ErrorsController < ApplicationController
+    skip_before_action :authenticate_user!, :raise => false
+    #skip_before_action :authenticate_user!, only: [:reduce_url, :redirect_to_main_url]
+
+    def not_found
+        #respond_to do |format|
+        #format.html { render status: 404 }
+        #end
+    end
+
+    def unacceptable
+        respond_to do |format|
+        format.html { render status: 422 }
+        end
+    end
+
+    def internal_error
+        respond_to do |format|
+        format.html { render status: 500 }
+        end
+    end
 end
