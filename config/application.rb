11c322d9bcfa6491723ac1cf6a74c058cba208bf
Marvelous Akpotu <still4marvelous@gmail.com>
2020-01-03 22:49:41 +0100

    Add comment in the application.rb file to signify where I add custom error configuration.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Weatherapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # I added this for custom error pages.
     config.exceptions_app = self.routes
   end
 end
